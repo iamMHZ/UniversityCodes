58e9b310fee874caa4c6e528e7ccee17881071f5
Mohammad Hossein <iammhz77@gmail.com>
2019-11-16 10:32:55 +0330

    testing spiders : digi failed Amazon successful

--- crawler/crawler/spiders/Amazon_spider.py
+++ crawler/crawler/spiders/Amazon_spider.py
@@ -4,7 +4,7 @@ from scrapy.linkextractors import LinkExtractor
 
 
 class AmazonCrawler(CrawlSpider):
-    name = 'amazon_crawler'
+    name = 'amazon'
 
     start_urls = [
         'https://www.amazon.com/b/ref=s9_acss_bw_cg_KOTHLPCG_1a1_w?node=565108&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-6&pf_rd_r=0RKF7FVNP517GE39J5QA&pf_rd_t=101&pf_rd_p=1e1598d2-28c3-4a64-91af-254d7a033ada&pf_rd_i=541966']
@@ -15,10 +15,10 @@ class AmazonCrawler(CrawlSpider):
         names = response.css('.a-size-large::text').extract()
         prices = response.css('.a-price-whole::text').extract()
 
-        # for price in prices:
-        #     print('\nPRICE : ' + str(price))
-        #     with open('AmazonPrices.txt' ,'a') as f  :
-        #         f.write( str(price) +'\n')
+        for price in prices:
+            print('\nPRICE : ' + str(price))
+            with open('AmazonPrices.txt', 'a') as f:
+                f.write(str(price) + '\n')
 
         for name in names:
             print('\nNMAE: ' + name + '\n')
--- crawler/crawler/spiders/Dijikala.py
+++ crawler/crawler/spiders/Dijikala.py
@@ -3,14 +3,14 @@ from scrapy.linkextractors import LinkExtractor
 
 
 class AmazonCrawler(CrawlSpider):
-    name = 'digikala'
+    name = 'digi'
 
     start_urls = ['https://www.digikala.com/search/category-notebook-netbook-ultrabook/?pageno=1&sortby=4']
 
     rules = (Rule(LinkExtractor(), callback='parser'),)
 
     def parser(self, response):
-        names = response.css('.c-product-box__title::text').extract()
+        names = response.css('.c-price__value-wrapper::text').extract()
         # prices = response.css('.a-price-whole::text').extract()
 
         # for price in prices:

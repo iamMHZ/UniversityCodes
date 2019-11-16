10b3dd7219c8283a579886f45a5caeededca3fee
Mohammad Hossein <iammhz77@gmail.com>
2019-11-16 20:27:09 +0330

    UNI Spider FINISHED :)

--- crawler/crawler/spiders/proj_scrapy.py
+++ crawler/crawler/spiders/proj_scrapy.py
@@ -1,16 +1,50 @@
 import scrapy
+import csv
 
 
 class Proj_Spider(scrapy.Spider):
     name = "sokhanha"
-    start_urls = [
-        'http://sokhanha.ir/'
-    ]
+
+    def __init__(self):
+        self.counter = 0
+        self.depth = 5
+
+    def start_requests(self):
+        urls = [
+            'http://sokhanha.ir/'
+        ]
+
+        for url in urls:
+            if self.counter < self.depth:
+                yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for quote in response.css('article'):
-            yield {
-                'title': quote.css('h1.entry-title a::text').get(),
+        self.counter += 1
 
-                'body': quote.css('div.entry-content p::text').getall()
-            }
+        self.log("  COUNTER : " + str(self.counter))
+
+        inner_urls = response.css('.page-numbers::attr(href)')
+
+        for url in inner_urls:
+            # print("\nURL:" + str(url))
+            url = str(url).split('=')[2][:-1]
+            # print("\nURL 2 :" + str(url))
+
+            url = f'{url[1:-1]}'
+
+            response.urljoin(url)
+            if self.counter < self.depth:
+                yield scrapy.Request(url, callback=self.parse)
+
+        for quote in response.css('article'):
+            yield {'title': quote.css('h1.entry-title a::text').get(),
+                   'content': quote.css('div.entry-content p::text').getall()}
+
+            # title = quote.css('h1.entry-title a::text').get()
+            # content = quote.css('div.entry-content p::text').getall()
+            # fields = ['عنوان', 'متنن']
+            # rows = [title, content]
+            # with open('sokhnha.txt', 'a') as f:
+            #     # f.write("TITLE : " + str(title) + "\nCONTENT: " + str(content) + '\n')
+            #     csvwriter = csv.writer(f)
+            #     csvwriter.writerow(rows)
